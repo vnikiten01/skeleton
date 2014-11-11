b32d1d50b168f7d547777d35a29b97e31048d9a4
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-11 18:09:54 -0500

    user login and sign up working next step logout

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -6,6 +6,30 @@ get '/login' do
   erb :login
 end
 
-get '/signup' do
-  erb :signup
+post '/login' do
+	@user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+	if @user
+		session[:id]= @user.id
+		redirect to('/')
+	else
+		set_error "Username or Password Incorrect"
+		redirect to('/login')
+	end
+end
+
+get '/sign_up' do
+	@user=User.new;
+	erb :sign_up
+end
+
+post '/sign_up' do
+ p @user = User.create(params[:user])
+  if @user.id
+  	session[:id] = @user.id
+  	redirect to('/')
+  else
+  	set_error('Wrong Email or Password entered')
+  	redirect to ('/sign_up')
+  # @user = User.create()
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
-  has_secure_password
-  # Remember to create a migration!
+	has_secure_password
+  	validates :name, :length => { :minimum => 3, :message => "must be at least 3 characters, fool!" }
+  	validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
 end
